674c2042c6fcb21462c29d59c7156a00672a729d
izdan777 <hassanjaufar77@gmail.com>
2026-01-31 13:34:40 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ function goToProducts() {
   window.location.href = "products.html";
 }
 
+function toggleMenu() {
+  document.getElementById("navLinks").classList.toggle("show");
+}
+
 document.querySelectorAll("form").forEach(form => {
   form.addEventListener("submit", e => {
     e.preventDefault();
-    alert("Form submitted successfully (backend coming soon)");
+    alert("Message received — we will contact you soon.");
     form.reset();
   });
 });
